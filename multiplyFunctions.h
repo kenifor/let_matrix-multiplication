59574608825bb0ff4a4e4c83a5f786a02086ca3e
kenifor <113609915+kenifor@users.noreply.github.com>
2022-11-14 14:46:36 +0300

    Delete multiplyFunctions.h

--- multiplyFunctions.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <vector>
-
-typedef std::vector<std::vector<int>> Matrix;
-
-Matrix multiplyTwoMatrixes(const Matrix & M1,const Matrix & M2);
-
-void printMatrix(const Matrix & matrix);
